14079eb45eff43235978c881214b94ac215d3f93
ankitsinggh007 <as3824115@gmail.com>
2023-05-06 21:51:21 +0530

    added mongo uri

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const Connect = require('./config/databseConfig');
 
-require('dotenv').config({path:"api/config/Config.env"});
+require('dotenv').config();
 
 const app=require('./app');
 
@@ -9,26 +9,8 @@ const app=require('./app');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const connect=async()=>{
     try {
-        console.log(process.env.Mongo_URI)
         await Connect(process.env.Mongo_URI);
         app.listen(process.env.PORT,()=>{
             console.log(`development server started on ${process.env.PORT}`);
