7b099ed1001006376ab97e45359cef1a44cc5441
ankitsinggh007 <as3824115@gmail.com>
2023-05-15 17:49:03 +0530

    cat

--- api/index.js
+++ api/index.js
@@ -19,10 +19,7 @@ app.use((req, res, next) => {
     next();
   });
   
-  app.use((req, res, next) => {
-    res.header('Set-Cookie', 'mycookie=value; SameSite=None; Secure');
-    next();
-  });
+  
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
