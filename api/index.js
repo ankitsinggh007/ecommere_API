033741c51b262335239fca2b3dbc113945b9542b
ankitsinggh007 <as3824115@gmail.com>
2023-05-15 17:34:28 +0530

    cors policy change

--- api/index.js
+++ api/index.js
@@ -9,7 +9,11 @@ const errorMidlleware=require('./middleware/error');
 const mainRoute=require('./routes');
 
 const app=express();
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }));
+  
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
