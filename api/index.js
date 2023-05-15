98f929292ee5a79daf2f290400bc9e77d944f1ef
ankitsinggh007 <as3824115@gmail.com>
2023-05-15 17:59:02 +0530

    merge

--- api/index.js
+++ api/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
   });
   
   
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
