1772e402c5bf63c7943cb084a06fe368c2a12a9f
ankitsinggh007 <as3824115@gmail.com>
2023-05-15 19:39:14 +0530

    margin

--- api/index.js
+++ api/index.js
@@ -10,7 +10,11 @@ const mainRoute=require('./routes');
 
 const app=express();
 
-app.use(cors());
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}));
