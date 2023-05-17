dc3ad1cba24096a05fc855474b9ac7381f727ea5
ankitsinggh007 <as3824115@gmail.com>
2023-05-17 21:26:40 +0530

    Revert "add body-parser"
    This reverts commit f58dbb3cf7629e92c35d400a84ba755ca72d4d6f.

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const corsConfig = {
   app.use(cors(corsConfig));
 app.use(cookieParser());
 app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+app.use(express.urlencoded({extended,limit: '50mb'}));
 
 
 app.use('/api',mainRoute);
