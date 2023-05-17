72613eae298028a3c2eea3f474ce6b2c45412923
ankitsinggh007 <as3824115@gmail.com>
2023-05-17 22:26:05 +0530

    addecors config with production ready value

--- api/index.js
+++ api/index.js
@@ -19,7 +19,8 @@ const app=express();
 
 
 const corsConfig = {
-    origin: ' http://localhost:5173',
+    // origin: ' http://localhost:5173',
+    origin:'https://brilliant-truffle-1cecdc.netlify.app/login',
     credentials: true,
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "*",
